e0e8aabf8a280441d73238b0b69531b42e95e7bc
Harshitha <harshithagalla00@gmail.com>
2025-10-02 13:27:00 +0000

    upated

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
+
 const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 8080;
-const HOST = '0.0.0.0'; //
 
 app.get("/", (req, res) => {
-  res.send("Hello Elastic Beanstalk! Code is deployed successfully from the Aws codepipeline");
+  res.send(" Hello Elastic Beanstalk! This Node.js app is deployed through codebuild and codepioeline successfully.");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
